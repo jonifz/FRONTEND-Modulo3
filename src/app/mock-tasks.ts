1862af31c536f396b0f7c8904aa3c8f1e1bebed3
jonifz <fernandezjonatan.m@gmail.com>
2023-01-01 23:55:38 -0300

    first commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,39 @@
+import {Task} from ".Task"
+
+export const TASK:Task [] = [
+    {
+        id:1,
+        text: "Saltar la soga",
+        day: 'Enero 2 a las 05:30',
+        reminder: true,
+
+    },
+    {
+        id:2,
+        text: "Termian el modudlo de Angular",
+        day: 'Enero 2 a las 06:30',
+        reminder: true,
+
+    },
+   {
+    id:3,
+        text: "Saltar la soga",
+        day: 'Enero 2 a las 17:30',
+        reminder: true,
+
+    },
+    {
+        id:4,
+            text: "Ir a la plaza",
+            day: 'Enero 2 a las 18:30',
+            reminder: true,
+    
+        },
+         {
+    id:5,
+        text: "Comprar para la cena",
+        day: 'Enero 2 a las 19:30',
+        reminder: true,
+
+    },
+]
